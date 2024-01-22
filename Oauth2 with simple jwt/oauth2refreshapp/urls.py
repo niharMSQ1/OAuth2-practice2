0836ff22b3719413a3eacb14592e4f3f11c78a2b
nihar ranjan mohanty <nihar.m@secqureone.com>
2024-01-22 15:44:37 +0530

    deleted

--- Oauth2 with simple jwt/oauth2refreshapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('register/', register, name = 'register'),
-    path('login/', login, name = 'login'),
-    path('callTheApi/', callTheApi, name = 'callTheApi'),
-]
--- calling 3rd party/dummyProjectApp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DummyprojectappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'dummyProjectApp'
